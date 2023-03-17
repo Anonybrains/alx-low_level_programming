0acff78902e5400d4f491d43098095dd513e31cb
Anonybrains <enweremchiexcel@gmail.com>
2023-03-16 22:32:12 -0700

    puts command

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The main executes the program.
+ *
+ * Return: is always 0
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
