7093358a772baeceb4bd55f7694be414073ebc22
Anonybrains <enweremchiexcel@gmail.com>
2023-03-22 14:53:37 -0700

    a function that prints all natural numbers from n to 98, followed by a new line.

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98- This the function name for printing natural no.
+ * @n: parameter for check
+ */
+
+void print_to_98(int n)
+	{
+	int x;
+
+	if (n >= 0 && n <= 98)
+	{
+		for (x = n ; x <= 98 ; x++)
+		{
+			printf("%d, ", x);
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (x = n ; x >= 98 ; x--)
+		{
+			printf("%d, ", x);
+		}
+		printf("\n");
+	}
+	else if (n < 0)
+	{
+		for (x = n ; x <= 98; x++)
+		{
+		printf("%d, ", x);
+		}
+		printf("\n");
+	}
+
+	else if (n == 98)
+	{
+		printf("%d, ", n);
+	}
+}
