d0daba1343b1fdf0127b26c8dc82ff43348637c1
Anonybrains <enweremchiexcel@gmail.com>
2023-03-22 19:06:51 -0700

    a function that prints the 9 times table, starting with 0.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,37 @@
-#include
+#include "main.h"
+/**
+ * times_table- the function name for the program
+ *
+ * Return: void
+ */
 
 void times_table(void)
-	{ 
-	int x;
+{
+int x, y, z;
 
-	
-	
+	for (x = 0; x < 10; x++)
+	{
+	for (y = 0; y < 10; y++)
+	{
+		z = y * x;
+		if (y == 0)
+		{
+		_putchar(z + '0');
+		}
+	if (z < 10 && z != 0)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(z + '0');
 	}
+	else if (z >= 10)
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar((z / 10) + '0');
+	_putchar((z % 10) + '0');
+	}
+	}
+	_putchar('\n');
+	}
+}
