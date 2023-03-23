91921f725dc5323106aa4fd87636b35ad4b92143
Anonybrains <enweremchiexcel@gmail.com>
2023-03-23 10:32:38 -0700

    a function that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul- the function that multiplies two integers.
+ * @a: the parameter that is multiplied
+ * @b: the parameter that is multiplied
+ *
+ * Return: the product if successful
+ */
+
+int mul(int a, int b)
+{
+	return ((a * b));
+}
