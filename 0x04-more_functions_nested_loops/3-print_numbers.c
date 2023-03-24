b3e83978a4c24c32b9819919b4e00a794232fff8
Anonybrains <enweremchiexcel@gmail.com>
2023-03-24 12:56:51 -0700

    a function that prints the numbers, from 0 to 9, followed by a new line.

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
- * print_numbers- a function that prints the numbers, from 0 to 9, followed by a new line.
- * 
+ * print_numbers- a function that print number from 0-9
+ *
  * Return: void
  */
 
 void print_numbers(void)
 {
-	int i;
+	char c;
 
-	for (i >= '48' ; i <= '57' ; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(0);
+		_putchar(c);
 	}
-	_putchar('\n');	
+	_putchar('\n');
 }
+
