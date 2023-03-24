c386428d918ab49f2fc34330d655efc00f6bf73c
Anonybrains <enweremchiexcel@gmail.com>
2023-03-24 12:39:22 -0700

    a program that prints the numbers from 1 to 100, followed by a new line. But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz.
    Each number or word should be separated by a space
    You are allowed to use the standard library

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the numbers from 1 to 100, followed by a new line
- * but for multiples of three prints Fizz instead of the number
- * and for the multiples of five prints Buzz
- * Return: Always 0 (Success)
+ * main - a program that prints the numbers from 1 to 100, followed byby a new line.
+ * But for multiples of three print Fizz instead of the number and for the multiples
+ * of five print Buzz. For numbers which are multiples of both three and five print Fizz
+ * Buzz
+ * Return: 0 if successful
  */
 int main(void)
 {
