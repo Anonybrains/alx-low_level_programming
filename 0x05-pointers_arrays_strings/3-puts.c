a247408994bbee5bd005b7aded68d7eea21c69c0
Anonybrains <enweremchiexcel@gmail.com>
2023-03-27 18:56:32 -0700

    a function that prints a string, followed by a new line, to stdout.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * _puts - a function that prints a string, followed by a new line
+ * _puts - a function that prints a string, followed by a new line to stdout.
  * @str: parameter (string to print)
  */
 
 void _puts(char *str)
 {
-       	while (*str != '\0') 
+	while (*str != '\0')
 	{
-		_putchar(*str++); 
+		_putchar(*str++);
 	}
-	_putchar('\n'); 
+	_putchar('\n');
 }
 
