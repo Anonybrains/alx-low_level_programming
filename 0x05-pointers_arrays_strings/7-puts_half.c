9e201ee320857e1172a933c2a124ac5f66fd912e
Anonybrains <enweremchiexcel@gmail.com>
2023-03-28 17:18:38 -0700

    Write a function that prints half of a string, followed by a new line.
    Prototype: void puts_half(char *str);
    The function should print the second half of the string
    If the number of characters is odd, the function should print the last n characters of the string, where n = (length_of_the_string - 1) / 2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,19 +1,28 @@
-#innclude "main.h"
-
+#include "main.h"
+/**
+ * puts_half- a function that prints half of a string, followed by a new line.
+ * @str: the function parameter to be printed
+ * Return:  void
+ */
 
 void puts_half(char *str)
 {
-	int x, n longi;
-	longi = 0;
+	int x;
+	int lenght = 0;
 
-	for (a = 0; str[a] != '\0'; a++)
-		longi++;
-	n = (longi / 2);
-	if ((longi % 2) == 1)
-		n = ((longi + 1) / 2);
-	for (a = n; str[a] != '\0'; a++)
-		_putchar(str[a]);
-
+	for (x = 0 ; str[x] != '\0' ; x++)
+		lenght++;
+	if (lenght % 2 == 0)
+	{
+	for (x = lenght / 2 ; str[x] != '\0' ; x++)
+		_putchar(str[x]);
 	_putchar('\n');
+	}
+	else if (lenght % 2 != 0)
+	{
+		for (x = (lenght - 1) / 2 ; str[x] != '\0' ; x++)
+			_putchar(str[x]);
+		_putchar('\n');
+	}
 
 }
